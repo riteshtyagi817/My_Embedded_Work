56c1cd089177944bd6fde5847a8cdfb47c4d404f
Ritesh Tyagi <riteshtyagi725@gmail.com>
2024-09-22 09:29:17 +0530

    added more work on daily basis

---
+++ CPlusPlus/C++_Programming/polymorphism_overloading/overlessthan/headers.h
@@ -0,0 +1,11 @@
+#ifndef HEADERS_H
+#define HEADERS_H
+#include<iostream>
+#include<string>
+#include<vector>
+using std::cout;
+using std::cin;
+using std::endl;
+using std::string;
+using std::vector;
+#endif
