0037eb981b4478b12634029e133e7837c22aca4c
Ritesh Tyagi <riteshtyagi725@gmail.com>
2024-12-30 21:27:47 +0530

    added mor work on daily basis

--- C_Project/decompression4.c
+++ C_Project/decompression4.c
@@ -11,12 +11,22 @@ void * decompression4(void *arg)
 	unsigned char ch;
 	unsigned char copy;
 	int bytes_read = 0;
+	int bytes_write = 0;
 	unsigned char readCh;
+	int wFd = 0;
         *status = EXIT_SUCCESS;
 	DCompress *dc = (DCompress *)arg;
 	int fileSize = lseek(dc->ifd, 0, SEEK_END);
 	printf("The compressed file size is %d\n",fileSize);
 	lseek(dc->ifd, 0, SEEK_SET);
+	// we will need to open the file in writing mode to write the output
+	printf("Enter the file name to write the decrypted output\n");
+	wFd = *(int *)(*funcPtr[6])((void *)"creating");
+	if(wFd < 0){
+		perror("could not open the file to write the decrypted output\n");
+		return (int *)(*funcPtr[5])((void *)"failure");
+	}
+
 	while(fileSize--){
 	
 		bytes_read = read(dc->ifd, &ch,1);
@@ -27,16 +37,27 @@ void * decompression4(void *arg)
 		copy = ch;
 		ch = ch >> 4;
 		int idx = (int)ch;
-		readCh = *(dc->ma + idx);
-		printf("character read is %c\n",readCh);
+		if(idx >= 0 && idx < 15){
+			readCh = *(dc->ma + idx);
+			printf("character read is %c\n",readCh);
+			bytes_write = write(wFd,&readCh,1);
+			if(bytes_write <= 0){
+				printf("could not write any bytes\n");
+
+			}
+
+		}
 		copy = copy & 0x0F;
 		idx = (int)copy;
-		readCh = *(dc->ma + idx);
-		printf("character read is %c\n",readCh);
+		if(idx >= 0 && idx < 15){
+			readCh = *(dc->ma + idx);
+			printf("character read is %c\n",readCh);
+			bytes_write = write(wFd,&readCh,1);
+			if(bytes_write <= 0){
+				printf("could not write any bytes\n");
 
-
-
-			
+			}
+		}
 
 
 	}
