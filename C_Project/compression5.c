63ad85badb9fa5188115660bf0752a7cfc5bf51a
Ritesh Tyagi <riteshtyagi725@gmail.com>
2025-01-11 09:12:51 +0530

    added more work on daily basis

--- C_Project/compression5.c
+++ C_Project/compression5.c
@@ -10,7 +10,10 @@ void * compression5(void *arg)
 	int fs = 0;
 	unsigned char byte = 0;
 	MUnique input;
-
+	int writeFd = 0;
+	unsigned char ch = '\0';
+	int read_bytes = 0;
+	unsigned char c = '\0';
  	status = (int *)malloc(1*sizeof(int));
         if(!status){
                 perror("malloc failed in compression 5\n");
@@ -35,6 +38,72 @@ void * compression5(void *arg)
 	// from the file and then write the encoding part to another file
 	// so need to  open the file in create mode for compressed file
 	printf("Please provide the compressed file name\n");
+ 	writeFd = *(int *)(*funcPtr[6])((void *)"creating");
+        if(writeFd < 0){
+                perror("File opening failed\n");         
+	}
+	unsigned char copy = 0;
+	while(input.size--){
+		
+
+		byte ^= byte;
+		read_bytes = (*(scmp->fileFd),&ch,1);
+		if(read_bytes <= 0){
+
+			printf("could not read anything from the file\n");
+			break;
+		}
+		input.ch = ch;
+                printf(" char passing %c\n",input.ch);
+                int *index = (int *)(*funcPtr[17])((void *)&input);
+                if(!index)
+                {
+                        perror("some issue finding the pos from getPos function\n");
+                        break;
+                }
+		c = *index;
+		c <<= 3;
+		byte |= c;
+		// need to check if this is the last byte we read 
+		// This handling I will do later on
+		
+
+
+		// now need to read another byte
+
+		read_bytes = (*(scmp->fileFd),&ch,1);
+		if(read_bytes <= 0){
+
+			printf("could not read anything from the file\n");
+			break;
+		}
+		input.ch = ch;
+                printf(" char passing %c\n",input.ch);
+                index = (int *)(*funcPtr[17])((void *)&input);
+                if(!index)
+                {
+                        perror("some issue finding the pos from getPos function\n");
+                        break;
+                }
+
+		c = *index;
+		copy = c;
+		c >>= 2;
+		byte |= c;
+		// byte has been formed
+		// printing the byte
+		int count = 8;
+		while(count--){
+
+			if(byte & 1) printf("1");
+			else printf("0");
+			byte >>= 1;
+		}
+		break;
+		
+
+
+	}
 
 
 
